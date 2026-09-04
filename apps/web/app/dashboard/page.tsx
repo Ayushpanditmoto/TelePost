270d4caecda63ba5fc51accc2ab8ff4266b25863
Ayush Pandit <ayushpanditmoto@gmail.com>
2026-09-04 19:26:52 +0530

    fix: restore message details panel layout

--- apps/web/app/dashboard/page.tsx
+++ apps/web/app/dashboard/page.tsx
@@ -1,16 +1,24 @@
 'use client'
 
+import styled from 'styled-components'
 import CenterPanel from '@/components/dashboard/CenterPanel'
 import RightPanel from '@/components/dashboard/RightPanel'
 import { useDashboardStore } from '@/store/dashboardStore'
+
+const Workspace = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  min-width: 0;
+`
 
 export default function DashboardPage() {
   const { selectedPostId } = useDashboardStore()
 
   return (
-    <>
+    <Workspace>
       <CenterPanel />
       {selectedPostId && <RightPanel />}
-    </>
+    </Workspace>
   )
 }
